fd2cee09fd3626824ea5f65ff796f33d971be7c6
genesislab-daeyun <108648002+genesislab-daeyun@users.noreply.github.com>
2022-08-05 21:48:16 +0900

    Create test_kube.py

---
+++ test_kube.py
@@ -0,0 +1,17 @@
+from airflow.models import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+
+k = KubernetesPodOperator(
+    name="hello-dry-run",
+    image="debian",
+    cmds=["bash", "-cx"],
+    arguments=["echo", "10"],
+    labels={"foo": "bar"},
+    annotations={"testKey": "testValue1"},
+    task_id="dry_run_demo",
+    do_xcom_push=True,
+)
+
+k.dry_run()
