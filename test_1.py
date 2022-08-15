aad53d250523203a58821f6ee7a0d342df3efc15
genesislab-daeyun <108648002+genesislab-daeyun@users.noreply.github.com>
2022-08-16 01:13:33 +0900

    test

--- test_1.py
+++ test_1.py
@@ -99,26 +99,52 @@ dag = DAG(
 #     affinity=affinity,
 # )
 
-t1 = KubernetesPodOperator(
-    name='print_date',
-    task_id='print_date',
-    image='debian',
-    cmds=["date"],
+# t1 = KubernetesPodOperator(
+#     name='print_date',
+#     task_id='print_date',
+#     image='debian',
+#     cmds=["date"],
+#     dag=dag,
+#     affinity=affinity,
+#     get_logs=True,
+# )
+
+kubernetes_min_pod = KubernetesPodOperator(
+    # The ID specified for the task.
+    task_id='pod-ex-minimum',
+    # Name of task you want to run, used to generate Pod ID.
+    name='pod-ex-minimum',
+    # Entrypoint of the container, if not specified the Docker container's
+    # entrypoint is used. The cmds parameter is templated.
+    cmds=['echo'],
+    # The namespace to run within Kubernetes, default namespace is
+    # `default`. There is the potential for the resource starvation of
+    # Airflow workers and scheduler within the Cloud Composer environment,
+    # the recommended solution is to increase the amount of nodes in order
+    # to satisfy the computing requirements. Alternatively, launching pods
+    # into a custom namespace will stop fighting over resources.
+    namespace='default',
+    # Docker image specified. Defaults to hub.docker.com, but any fully
+    # qualified URLs will point to a custom repository. Supports private
+    # gcr.io images if the Composer Environment is under the same
+    # project-id as the gcr.io images and the service account that Composer
+    # uses has permission to access the Google Container Registry
+    # (the default service account has permission)
+    image='gcr.io/gcp-runtimes/ubuntu_18_0_4',
     dag=dag,
     affinity=affinity,
-    get_logs=True,
 )
 
-t2 = KubernetesPodOperator(
-    name='sleep',
-    task_id='sleep',
-    image='debian',
-    cmds=["sleep", "5"],
-    retries=3,
-    dag=dag,
-    affinity=affinity,
-    get_logs=True,
-)
+# t2 = KubernetesPodOperator(
+#     name='sleep',
+#     task_id='sleep',
+#     image='debian',
+#     cmds=["sleep", "5"],
+#     retries=3,
+#     dag=dag,
+#     affinity=affinity,
+#     get_logs=True,
+# )
 
 # t2 = BashOperator(
 #     task_id='sleep',
@@ -162,5 +188,6 @@ templated_command = """
 
 # [END jinja_template]
 
-t1 >> [t2]
+# t1 >> [t2]
+kubernetes_min_pod
 # [END tutorial]
