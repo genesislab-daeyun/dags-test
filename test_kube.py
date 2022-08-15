fd203d0a6e0ff471e8fbe007ea1fb12585d01ec7
genesislab-daeyun <108648002+genesislab-daeyun@users.noreply.github.com>
2022-08-15 22:50:31 +0900

    test

--- test_kube.py
+++ test_kube.py
@@ -1,48 +1,48 @@
-from datetime import datetime
+# from datetime import datetime
 
-from airflow.models import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+# from airflow.models import DAG
+# from airflow.operators.dummy_operator import DummyOperator
+# from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 
-DEFAULT_DATE = datetime(2016, 1, 1)
+# DEFAULT_DATE = datetime(2016, 1, 1)
 
-test_affinity = {
-    'nodeAffinity': {
-        'requiredDuringSchedulingIgnoredDuringExecution': {
-            'nodeSelectorTerms': [{
-                'matchExpressions': [{
-                    'key': 'testKey',
-                    'operator': 'In',
-                    'values': [
-                        'testValue1'
-                    ]
-                }]
-            }]
-        }
-    }
-}
+# test_affinity = {
+#     'nodeAffinity': {
+#         'requiredDuringSchedulingIgnoredDuringExecution': {
+#             'nodeSelectorTerms': [{
+#                 'matchExpressions': [{
+#                     'key': 'testKey',
+#                     'operator': 'In',
+#                     'values': [
+#                         'testValue1'
+#                     ]
+#                 }]
+#             }]
+#         }
+#     }
+# }
 
-args = {
-    'owner': 'airflow',
-    'start_date': DEFAULT_DATE,
-}
+# args = {
+#     'owner': 'airflow',
+#     'start_date': DEFAULT_DATE,
+# }
 
-dag = DA11G(
-    dag_id='kubernetes-dag',
-    description='kubernetes pod operator',
-    default_args=args,
-    max_active_runs=1
-)
+# dag = DA11G(
+#     dag_id='kubernetes-dag',
+#     description='kubernetes pod operator',
+#     default_args=args,
+#     max_active_runs=1
+# )
 
-k = Kubernet11esPodOperator(
-    name="hello-dry-run",
-    image="debian",
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10"],
-    labels={"foo": "bar"},
-    annotations={"testkey": "testval"},
-    task_id="dry_run_demo",
-    dag=dag,
-    affinity=test_affinity,
-)
+# k = Kubernet11esPodOperator(
+#     name="hello-dry-run",
+#     image="debian",
+#     cmds=["bash", "-cx"],
+#     arguments=["echo", "10"],
+#     labels={"foo": "bar"},
+#     annotations={"testkey": "testval"},
+#     task_id="dry_run_demo",
+#     dag=dag,
+#     affinity=test_affinity,
+# )
     
